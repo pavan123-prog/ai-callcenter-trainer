3173eece66d492e83bc26b6769ea03a76e1ffdad
pavan123-prog <pavankumardeveege02@gmail.com>
2025-05-31 10:15:57 +0530

    Initial project setup

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send('AI Call Center Trainer Backend Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
